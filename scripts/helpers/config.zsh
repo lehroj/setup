97fe19b31f04ac40e66afdd2d3db6da8dc4de174
Jorhel Bourdier <lehroj@pm.me>
2024-12-28 23:49:48 +0100

    Update.

--- scripts/helpers/config.zsh
+++ scripts/helpers/config.zsh
@@ -4,30 +4,35 @@ function symlink_file()
 {
     local SOURCE_FILE=$1
     local TARGET_FILE=$2
+    local TARGET_DIR=$(dirname "$TARGET_FILE")
 
     if [[ -L "$TARGET_FILE" || -f "$TARGET_FILE" ]];
     then
-        msg_info "File already exists for $(basename "$TARGET_FILE"). [o]verwrite ? any other key to skip."
+        msg_info "File already exists for '$TARGET_FILE'. [o]verwrite? any other key to skip."
         read -ks REPLY
 
         if [[ "$REPLY" == "o" || "$REPLY" == "O" ]];
         then
             rm -rf "$TARGET_FILE"
             ln -s "$SOURCE_FILE" "$TARGET_FILE"
-            msg_success "File overwritten for $(basename "$TARGET_FILE")."
+            msg_success "File overwritten for '$TARGET_FILE'."
         else
-            msg_success "Skipped for $(basename "$TARGET_FILE")."
+            msg_success "Skipped for '$TARGET_FILE'."
         fi
     else
-        msg_info "File $(basename "$TARGET_FILE") doesn't exist. [c]reate ? any other key to skip."
+        msg_info "File '$TARGET_FILE' doesn't exist. [c]reate? any other key to skip."
         read -ks REPLY
 
         if [[ "$REPLY" == "c" || "$REPLY" == "C" ]];
         then
+            if [[ ! -d "$TARGET_DIR" ]]; then
+                mkdir -p "$TARGET_DIR"
+            fi
+
             ln -s "$SOURCE_FILE" "$TARGET_FILE"
-            msg_success "Symlink created for $(basename "$TARGET_FILE")."
+            msg_success "Symlink created for '$TARGET_FILE'."
         else
-            msg_success "Skipped for $(basename "$TARGET_FILE")."
+            msg_success "Skipped for '$TARGET_FILE'."
         fi
     fi
 
@@ -61,20 +66,20 @@ function config_git()
 
 function config_term()
 {
-    local HYPER_CONFIG=$SETUP/dotfiles/term/hyper.js
-    local HYPER_TARGET=$HOME/.hyper.js
+    local GHOSTTY_CONFIG=$SETUP/dotfiles/term/ghostty.conf
+    local GHOSTTY_TARGET=$HOME/.config/ghostty/config
     local STARSHIP_CONFIG=$SETUP/dotfiles/term/starship.toml
     local STARSHIP_TARGET=$HOME/.config/starship.toml
 
     echo
 
-    if [[ ! -f "$HYPER_CONFIG" ]];
+    if [[ ! -f "$GHOSTTY_CONFIG" ]];
     then
-        msg_error "Hyper config file doesn't exist: $HYPER_CONFIG"
+        msg_error "Ghostty config file doesn't exist: $GHOSTTY_CONFIG"
         return 1
     fi
 
-    symlink_file "$HYPER_CONFIG" "$HYPER_TARGET"
+    symlink_file "$GHOSTTY_CONFIG" "$GHOSTTY_TARGET"
 
     if [[ ! -f "$STARSHIP_CONFIG" ]];
     then
